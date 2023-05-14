99de58a9020282a4e116365d4029d5c949cc261b
tobsdev12 <tobbytobs1@gmail.com>
2023-05-14 15:57:15 +0100

    added create route

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebApiService } from '../Service/web-api.service';
 
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
   styleUrls: ['./add-product.component.css']
 })
-export class AddProductComponent {
+export class AddProductComponent implements OnInit {
+  product = {
+    name: '',
+    description: '',
+    price: 0
+  }
+  submitted = false;
+  constructor(private productService: WebApiService){}
 
+  ngOnInit(): void {
+      
+  }
+
+  createProduct(){
+    const data = {
+      name: this.product.name,
+      description: this.product.description,
+      price: this.product.price
+    };
+    
+    this.productService.addProduct(data)
+      .subscribe(
+        response => {
+          console.log(response)
+          this.submitted = true
+        },
+       
+      )
+  }
+
+  newProduct(): void{
+    this.submitted=false
+    this.product ={
+      name: '',
+      description: '',
+      price: 0
+    }
+  }
+  
+  
 }
