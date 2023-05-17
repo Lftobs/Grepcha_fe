84948306bcbd206418f9f321f3a776f08069fdc0
Lftobs <tobbytobs1@email.com>
2023-05-18 00:15:43 +0100

    signups fe

--- src/app/Service/web-api.service.ts
+++ src/app/Service/web-api.service.ts
@@ -2,38 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs'
 
-//const baseUrl="https://node-be-eta.vercel.app/api/products/"
-const baseUrl="http://127.0.0.1:8080/api/products/"
+const baseUrl="https://node-be-eta.vercel.app/api/products/"
+//const baseUrl="http://127.0.0.1:8080/api/products/"
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebApiService {
-  myItems;
-  constructor(private http: HttpClient) {
-    this.myItems = [
-      {
-        name: 'product 1',
-        description: 'this is product 1',
-        id: 1
-      },
-      {
-        name: 'product 2',
-        description: 'this is product 2',
-        id: 2
-      },
-      {
-        name: 'product 3',
-        description: 'this is product 3',
-        id: 3
-      },
-      {
-        name: 'product 4',
-        description: 'this is product 4',
-        id: 4
-      }
-    ]
-   }
+
+  constructor(private http: HttpClient) {}
 
   getProduct() {
     return this.http.get(`${baseUrl}`, { withCredentials: true });
