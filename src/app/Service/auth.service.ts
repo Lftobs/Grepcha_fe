51ab095c77c5aa64d3a23688585d2da4b85fff63
Lftobs <tobbytobs1@email.com>
2023-05-17 23:22:16 +0100

    login fe: v1

--- src/app/Service/auth.service.ts
+++ src/app/Service/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-const baseUrl="https://node-be-eta.vercel.app/api/auth/"
-//const baseUrl="http://127.0.0.1:8080/api/auth/"
+//const baseUrl="https://node-be-eta.vercel.app/api/auth/"
+const baseUrl="http://127.0.0.1:8080/api/auth/"
 
 @Injectable({
   providedIn: 'root'
