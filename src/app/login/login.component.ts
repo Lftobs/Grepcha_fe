809573ec309405149f4ac2819dc09e9db87e9321
Lftobs <tobbytobs1@email.com>
2023-05-17 15:06:49 +0100

    login fe

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../Service/auth.service';
 import { ActivatedRoute, Route, Router } from '@angular/router'
 
 /*type IsError = {
@@ -11,41 +12,82 @@ import { ActivatedRoute, Route, Router } from '@angular/router'
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit{
-  login = {
+  data = {
       username: '',
       password: ''
   }
-  
+  pwd: any
   error = {
       username: '',
       password: '',
+      pwdErr: ''
   }
   
-  constructor(private router: Router){}
+  constructor(private router: Router, private auth: AuthService){}
     
   ngOnInit(): void {
       
   }
   
-  signIn() {
-      if (!/^.{5,}$/.test(this.login.username)){
+  signIn(): any {
+      if (!/^.{5,}$/.test(this.data.username)){
           this.error.username = 'Invalid username'
-          return this.error.username
+	  return this.error.username
       } else {
           this.error.username = ''
       }
-      if (!/^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/.test(this.login.password)) {
+      if (!/^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/.test(this.data.password)) {
           this.error.password= 'Invalid Password'
-	  alert(this.login.password)
-          return this.error.password
+	  return this.error.password
       } else {
           this.error.password = ''
       }
 
-      localStorage.setItem('isAuth', 'true')
-      this.router.navigate(['home'])
-      return alert('success')
+      this.auth.signIn(this.data)
+	.subscribe(
+	    {
+		next: res => {
+		  console.log(res)
+		  localStorage.setItem('isAuth', 'true')
+		  this.router.navigate(['/'])
+		},
+
+		error: (e) => console.log(e)
+	    }
+	    
+	)
       
+      
+  }
+
+
+  signUp(): any{
+     if (!/^.{5,}$/.test(this.data.username)){
+         this.error.username = 'Invalid username'
+	 return this.error.username
+     } else { this.error.username='' }
+
+     if (!/^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/.test(this.data.password)) {
+         this.error.password= 'Invalid Password'
+	 return this.error.password
+     } else { this.error.password = '' }
+     if (this.data.password != this.pwd){
+     	this.error.pwdErr = 'Password does not match'
+	return this.error.pwdErr
+     } else { this.error.pwdErr = '' }
+
+     this.auth.signUp(this.data)
+       .subscribe(
+	  {
+	      next: res => {
+	          console.log(res)
+		  this.router.navigate(['log-in'])
+	      },
+
+	      error: (e) => console.log(e)
+	  }
+	  
+       )
   }
 
 }
